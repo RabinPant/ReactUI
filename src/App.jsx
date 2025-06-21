7301ab4cfb7acd5d31d68013d72106a36f7e4531
= <pantrabin12@gmail.com>
2025-06-21 11:38:26 -0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./Components/Header";
-
+import Footer from "./Components/footer/Footer";
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
