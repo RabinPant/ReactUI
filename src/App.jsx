8d5a0beb5fd846abd4846eb697f22f30fd87b77d
= <pantrabin12@gmail.com>
2025-06-22 11:02:09 -0400

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/footer/Footer";
+import Home from "./Components/Home";
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
---
+++ src/Components/PageHeading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PageTitle from "./PageTitle";
+export default function PageHeading() {
+  return (
+    <div>
+      <PageTitle />
+    </div>
+  );
+}
