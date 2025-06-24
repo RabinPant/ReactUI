4617f453fcca23e5c7901a0c314056bcba0817c5
= <pantrabin12@gmail.com>
2025-06-23 23:14:46 -0400

    added children and props

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,12 @@ import PageHeading from "./PageHeading";
 export default function Home() {
   return (
     <div className="home-container">
-      <PageHeading />
+      <PageHeading title="Explore Eazy Stickers">
+        <p className="page-heading-paragraph">
+          Add a touch of creativity to your space with our wide range of fun and
+          unique stickers. Perfect for any occasion
+        </p>
+      </PageHeading>
     </div>
   );
 }
--- src/Components/PageHeading.jsx
+++ src/Components/PageHeading.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PageTitle from "./PageTitle";
-export default function PageHeading() {
+export default function PageHeading({ children, title }) {
   return (
-    <div>
-      <PageTitle />
+    <div className="page-heading-container">
+      <PageTitle title={title} />
+      {children}
     </div>
   );
 }
